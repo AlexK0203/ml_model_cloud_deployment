76cd1dde615a18fe7542a387a2c1b1abf752e965
Alexander Kindle <alexander.a.kindle@accenture.com>
2026-03-02 11:50:43 +0100

    loading data and cleaning whitespaces

--- starter/starter/ml/data.py
+++ starter/starter/ml/data.py
@@ -1,70 +1,93 @@
 import numpy as np
 from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
+import pandas as pd
+import logging
+
+# Configure logging to show the time and the message
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+# Loading Data
+
+df = pd.read_csv("data/census.csv")
+
+# Clean header names from whitespace
+df.columns = df.columns.str.strip()
+# clean Column names from whitespaces
+df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+# Logging
+logger.info("Success: headers cleaned and removed whitespaces from rows.")
+# Save cleaned dataframe
+df.to_csv('starter/data/clean_census.csv', index=False) # index false, so clean file has the exact same structure as the original, just without the messy spaces.
+
+# Logging
+logger.info("Success: Data saved to clean_census.csv.")
 
 
-def process_data(
-    X, categorical_features=[], label=None, training=True, encoder=None, lb=None
-):
-    """ Process the data used in the machine learning pipeline.
 
-    Processes the data using one hot encoding for the categorical features and a
-    label binarizer for the labels. This can be used in either training or
-    inference/validation.
+# def process_data(
+#     X, categorical_features=[], label=None, training=True, encoder=None, lb=None
+# ):
+#     """ Process the data used in the machine learning pipeline.
 
-    Note: depending on the type of model used, you may want to add in functionality that
-    scales the continuous data.
+#     Processes the data using one hot encoding for the categorical features and a
+#     label binarizer for the labels. This can be used in either training or
+#     inference/validation.
 
-    Inputs
-    ------
-    X : pd.DataFrame
-        Dataframe containing the features and label. Columns in `categorical_features`
-    categorical_features: list[str]
-        List containing the names of the categorical features (default=[])
-    label : str
-        Name of the label column in `X`. If None, then an empty array will be returned
-        for y (default=None)
-    training : bool
-        Indicator if training mode or inference/validation mode.
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained sklearn OneHotEncoder, only used if training=False.
-    lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained sklearn LabelBinarizer, only used if training=False.
+#     Note: depending on the type of model used, you may want to add in functionality that
+#     scales the continuous data.
 
-    Returns
-    -------
-    X : np.array
-        Processed data.
-    y : np.array
-        Processed labels if labeled=True, otherwise empty np.array.
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained OneHotEncoder if training is True, otherwise returns the encoder passed
-        in.
-    lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained LabelBinarizer if training is True, otherwise returns the binarizer
-        passed in.
-    """
+#     Inputs
+#     ------
+#     X : pd.DataFrame
+#         Dataframe containing the features and label. Columns in `categorical_features`
+#     categorical_features: list[str]
+#         List containing the names of the categorical features (default=[])
+#     label : str
+#         Name of the label column in `X`. If None, then an empty array will be returned
+#         for y (default=None)
+#     training : bool
+#         Indicator if training mode or inference/validation mode.
+#     encoder : sklearn.preprocessing._encoders.OneHotEncoder
+#         Trained sklearn OneHotEncoder, only used if training=False.
+#     lb : sklearn.preprocessing._label.LabelBinarizer
+#         Trained sklearn LabelBinarizer, only used if training=False.
 
-    if label is not None:
-        y = X[label]
-        X = X.drop([label], axis=1)
-    else:
-        y = np.array([])
+#     Returns
+#     -------
+#     X : np.array
+#         Processed data.
+#     y : np.array
+#         Processed labels if labeled=True, otherwise empty np.array.
+#     encoder : sklearn.preprocessing._encoders.OneHotEncoder
+#         Trained OneHotEncoder if training is True, otherwise returns the encoder passed
+#         in.
+#     lb : sklearn.preprocessing._label.LabelBinarizer
+#         Trained LabelBinarizer if training is True, otherwise returns the binarizer
+#         passed in.
+#     """
 
-    X_categorical = X[categorical_features].values
-    X_continuous = X.drop(categorical_features, axis=1)
+#     if label is not None:
+#         y = X[label]
+#         X = X.drop([label], axis=1)
+#     else:
+#         y = np.array([])
 
-    if training is True:
-        encoder = OneHotEncoder(sparse_output=False, handle_unknown="ignore")
-        lb = LabelBinarizer()
-        X_categorical = encoder.fit_transform(X_categorical)
-        y = lb.fit_transform(y.values).ravel()
-    else:
-        X_categorical = encoder.transform(X_categorical)
-        try:
-            y = lb.transform(y.values).ravel()
-        # Catch the case where y is None because we're doing inference.
-        except AttributeError:
-            pass
+#     X_categorical = X[categorical_features].values
+#     X_continuous = X.drop(categorical_features, axis=1)
 
-    X = np.concatenate([X_continuous, X_categorical], axis=1)
-    return X, y, encoder, lb
+#     if training is True:
+#         encoder = OneHotEncoder(sparse_output=False, handle_unknown="ignore")
+#         lb = LabelBinarizer()
+#         X_categorical = encoder.fit_transform(X_categorical)
+#         y = lb.fit_transform(y.values).ravel()
+#     else:
+#         X_categorical = encoder.transform(X_categorical)
+#         try:
+#             y = lb.transform(y.values).ravel()
+#         # Catch the case where y is None because we're doing inference.
+#         except AttributeError:
+#             pass
+
+#     X = np.concatenate([X_continuous, X_categorical], axis=1)
+#     return X, y, encoder, lb
