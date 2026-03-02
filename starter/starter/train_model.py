481ca66903fdda3256d06a17c3071fc886420f52
Alexander Kindle <alexander.a.kindle@accenture.com>
2026-03-02 13:00:35 +0100

    loaded and cleaned data from whitespaces

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,8 +1,28 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+import logging
+import pandas as pd
 
-# Add the necessary imports for the starter code.
+# Configure logging to show the time and the message
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+# Loading Data
+
+df = pd.read_csv("data/census.csv")
+
+# Clean header names from whitespace
+df.columns = df.columns.str.strip()
+# clean Column names from whitespaces
+df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+# Logging
+logger.info("Success: headers cleaned and removed whitespaces from rows.")
+# Save cleaned dataframe
+df.to_csv('data/clean_census.csv', index=False) # index false, so clean file has the exact same structure as the original, just without the messy spaces.
+
+# Logging
+logger.info("Success: Data saved to clean_census.csv.")
 
 # Add code to load in the data.
 
